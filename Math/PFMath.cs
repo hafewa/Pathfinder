e94d79fb66f556b2958b6747cf0fe3d9bba80a9a
德克 邓 <451274642@qq.com>
2020-05-09 22:20:33 +0800

    四叉树

---
+++ Math/PFMath.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace Pathfinder
+{
+	public struct PFPoint
+	{
+		public int x;
+		public int y;
+
+		public PFPoint(int x, int y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+	}
+	public struct PFRect
+	{
+		public int x;
+		public int y;
+		public int z;
+		public int w;
+
+		public PFRect(int x, int y, int z, int w)
+		{
+			this.x = x;
+			this.y = y;
+			this.z = z;
+			this.w = w;
+		}
+	}
+}
---
+++ QuadTrees/PFQuadNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+
+namespace Pathfinder
+{
+	public interface PFIQuadNode
+    {
+		
+    }
+
+	public class PFQuadCircle : PFIQuadNode
+	{
+		public PFQuadCircle(PFPoint point, int radius)
+		{
+
+		}
+	}
+}
